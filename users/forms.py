5106c44fd8af83d9d71a1a01973d4b5271ad4141
rakibIV <rakibhasan42008@gmail.com>
2025-03-29 00:19:57 +0600

    Frontend url changed to render

--- users/forms.py
+++ users/forms.py
@@ -8,7 +8,7 @@ class StyledFormMixin:
         super().__init__(*args, **kwargs)
         self.apply_styled_widgets()
         
-    default_classes = "w-full px-3 py-2 border rounded border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 m-3"
+    default_classes = "w-full p-3 border rounded border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 m-3 mb-5"
     
     def apply_styled_widgets(self):
         for field_name, field in self.fields.items():
