dc556ca335b380ea9be9c0ef5bb216722ebbcebb
rakibIV <rakibhasan42008@gmail.com>
2025-03-28 16:34:50 +0600

    Data base sirectory change

--- event_management/settings.py
+++ event_management/settings.py
@@ -94,25 +94,25 @@ WSGI_APPLICATION = 'event_management.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME',default=''),
-        'USER': config('DB_USER',default=''),
-        'PASSWORD': config('DB_PASSWORD',default=''),
-        'HOST': config('DB_HOST',default='localhost'),
-        'PORT': config('DB_PORT',cast=int),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('DB_NAME',default=''),
+#         'USER': config('DB_USER',default=''),
+#         'PASSWORD': config('DB_PASSWORD',default=''),
+#         'HOST': config('DB_HOST',default='localhost'),
+#         'PORT': config('DB_PORT',cast=int),
+#     }
+# }
 
 # Replace the SQLite DATABASES configuration with PostgreSQL:
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         # Replace this value with your local database's connection string.
-#         default='postgresql://event_manager_db_7v26_user:yuVU3mXzafaluZ4x6BiJx3h1Q0KsJlJb@dpg-cuedk81opnds738godd0-a.oregon-postgres.render.com/event_manager_db_7v26',
-#         conn_max_age=600
-#     )
-# }
+DATABASES = {
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://event_manager_db_7v26_user:yuVU3mXzafaluZ4x6BiJx3h1Q0KsJlJb@dpg-cuedk81opnds738godd0-a.oregon-postgres.render.com/event_manager_db_7v26',
+        conn_max_age=600
+    )
+}
 
 
 # Password validation
