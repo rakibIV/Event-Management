28cf1b0df4ead760b627ab6d04c3b877a2a07f25
rakibIV <rakibhasan42008@gmail.com>
2025-04-20 08:45:20 +0600

    database reset done

--- event_management/settings.py
+++ event_management/settings.py
@@ -95,25 +95,25 @@ WSGI_APPLICATION = 'event_management.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME',default=''),
-        'USER': config('DB_USER',default=''),
-        'PASSWORD': config('DB_PASSWORD',default=''),
-        'HOST': config('DB_HOST',default='localhost'),
-        'PORT': config('DB_PORT',cast=int),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('DB_NAME',default=''),
+#         'USER': config('DB_USER',default=''),
+#         'PASSWORD': config('DB_PASSWORD',default=''),
+#         'HOST': config('DB_HOST',default='localhost'),
+#         'PORT': config('DB_PORT',cast=int),
+#     }
+# }
 
 # Replace the SQLite DATABASES configuration with PostgreSQL:
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         # Replace this value with your local database's connection string.
-#         default='postgresql://event_manager_db_cdx7_user:RFN8ZN5TZo9RnNwZXjD0mCB12tUgPebR@dpg-cvjd9sidbo4c73akd9fg-a.oregon-postgres.render.com/event_manager_db_cdx7',
-#         conn_max_age=600
-#     )
-# }
+DATABASES = {
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://event_manager_db_l8ln_user:JkfRjF1qwrJVVaRKF58xSYoHjy1YftRs@dpg-d025re3uibrs73ai8u6g-a.oregon-postgres.render.com/event_manager_db_l8ln',
+        conn_max_age=600
+    )
+}
 
 
 # Password validation
@@ -170,7 +170,7 @@ EMAIL_PORT = config('EMAIL_PORT',cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
-# FRONTEND_URL = 'https://event-management-gmrf.onrender.com'
-FRONTEND_URL = 'http://127.0.0.1:8000/'
+FRONTEND_URL = 'https://event-management-gmrf.onrender.com'
+# FRONTEND_URL = 'http://127.0.0.1:8000/'
 
 LOGOUT_REDIRECT_URL = 'sign-in'
