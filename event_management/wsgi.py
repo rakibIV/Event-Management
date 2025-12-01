1b357e8a23eb7283452888fbbca8940bf8c269a3
Rakib Hossain <113041656+rakibIV@users.noreply.github.com>
2025-12-02 00:24:35 +0600

    Run migrations on WSGI application startup
    Add automatic migration on WSGI startup

--- event_management/wsgi.py
+++ event_management/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'event_management.settings')
-
+# Run migrations on startup
+call_command('migrate', interactive=False)
 application = get_wsgi_application()
